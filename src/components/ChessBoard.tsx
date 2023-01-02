550e502de474a97db489d871b755684c0898df3e
NightCreature7 <81756425+NightCreature7@users.noreply.github.com>
2023-01-02 11:12:10 +0100

    Fixed Client - Side Error

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Box } from "@chakra-ui/layout";
 import React, { useCallback, useMemo, useState } from "react";
 import styled from "styled-components";
