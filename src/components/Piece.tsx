6ec46777a51d1772b948d866dfc9ed888b6dc94e
NightCreature7 <81756425+NightCreature7@users.noreply.github.com>
2023-01-02 12:55:27 +0100

    Fixed Production Bugs

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
-import Pieces from '../../common/pieces'
+import React from "react";
+import styled, { css } from "styled-components";
+import Pieces from "../../common/pieces";
 
 const PieceWrapper = styled.div<{ color: string }>`
   position: absolute;
@@ -19,57 +19,57 @@ const PieceWrapper = styled.div<{ color: string }>`
   color: ${(p) => p.color};
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: black;
-`
+`;
 
 function hasLowerCase(str: string) {
-  return /[a-z]/.test(str)
+  return /[a-z]/.test(str);
 }
 
 const calculateSpritePosition = (p: { piece: string; color: string }) => {
   let x = 0,
-    y = 0
+    y = 0;
 
   if (p.color === Pieces.BLACK) {
-    y = -60
+    y = -60;
   } else {
-    y = 0
+    y = 0;
   }
 
-  if (p.piece === 'K') {
-    x = 0
+  if (p.piece === "K") {
+    x = 0;
   }
 
-  if (p.piece === 'Q') {
-    x = -60
+  if (p.piece === "Q") {
+    x = -60;
   }
 
-  if (p.piece === 'B') {
-    x = -120
+  if (p.piece === "B") {
+    x = -120;
   }
 
-  if (p.piece === 'N') {
-    x = -180
+  if (p.piece === "N") {
+    x = -180;
   }
 
-  if (p.piece === 'R') {
-    x = -240
+  if (p.piece === "R") {
+    x = -240;
   }
 
-  if (p.piece === 'P') {
-    x = -300
+  if (p.piece === "P") {
+    x = -300;
   }
 
-  x = x * 8
-  y = y * 8
+  x = x * 8;
+  y = y * 8;
 
   return css`
     object-position: ${x}px ${y}px;
-  `
-}
+  `;
+};
 
-const PieceIcon = styled.img.attrs({ src: 'assets/sprites/classic.png' }) <{
-  piece: string
-  color: string
+const PieceIcon = styled.img.attrs({ src: "assets/sprites/classic.png" })<{
+  piece: string;
+  color: string;
 }>`
   object-fit: none;
   object-position: 0 0;
@@ -83,21 +83,21 @@ const PieceIcon = styled.img.attrs({ src: 'assets/sprites/classic.png' }) <{
   }
 
   ${calculateSpritePosition};
-`
+`;
 
 const Piece: React.FC<{
-  piece: string
-  draggable: boolean
-  onDragStart: Function
-  onDragEnd: Function
+  piece: string;
+  draggable: boolean;
+  onDragStart: Function;
+  onDragEnd: Function;
 }> = ({ piece, draggable, onDragStart, onDragEnd }) => {
-  const color = hasLowerCase(piece) ? Pieces.BLACK : Pieces.WHITE
+  const color = hasLowerCase(piece) ? Pieces.BLACK : Pieces.WHITE;
 
   return (
     <PieceWrapper color={color}>
       {piece && (
         <PieceIcon
-          piece={piece.toUpperCase()}
+          piece={piece.toUpperCase() || ""}
           color={color}
           draggable={draggable}
           onDragStart={() => onDragStart()}
@@ -105,7 +105,7 @@ const Piece: React.FC<{
         />
       )}
     </PieceWrapper>
-  )
-}
+  );
+};
 
-export default React.memo(Piece)
+export default React.memo(Piece);
